1fe5cc29674f69291be965207dca4f749109afd6
Michiel Post <michiel@q42.nl>
2019-07-26 10:51:20 +0200

    test editor and api controller

---
+++ src/QMS.Web/Controllers/ApiController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+
+namespace QMS.Web.Controllers
+{
+    [Route("api/")]
+    [ApiController]
+    public class ApiController : ControllerBase
+    {
+        [HttpPost]
+        [Route("save")]
+        public void Post([FromBody] JObject value)
+        {
+
+        }
+
+       
+    }
+}
