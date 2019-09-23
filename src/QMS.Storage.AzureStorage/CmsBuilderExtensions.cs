549898024a558e9650d117f9a9dbb4153723e002
Michiel Post <michiel@q42.nl>
2019-09-23 14:09:18 +0200

    removed unused aspnetcore references

--- src/QMS.Storage.AzureStorage/CmsBuilderExtensions.cs
+++ src/QMS.Storage.AzureStorage/CmsBuilderExtensions.cs
@@ -1,5 +1,4 @@
-﻿using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+﻿using Microsoft.Extensions.Configuration;
 using QMS.Storage.AzureStorage.Models;
 using Microsoft.Extensions.DependencyInjection;
 using System;
