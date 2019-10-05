9867e0ab8757f7c25818004cdba5faefe9f9b561
Michiel Post <michiel@q42.nl>
2019-10-05 19:58:30 +0200

    Use IWebHostEnvironment

--- src/QMS.Web/Startup.cs
+++ src/QMS.Web/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 using QMS.Core;
 using QMS.Models;
 using QMS.Services;
@@ -40,7 +41,7 @@ namespace QMS.Web
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
