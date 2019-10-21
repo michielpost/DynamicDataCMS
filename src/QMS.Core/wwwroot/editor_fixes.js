4e62f77fbda63b4e77636873ab2a727fdf6ce265
michielpost <michiel@q42.nl>
2019-10-21 10:19:13 +0200

    use autorefresh for markdown editor on second tab

--- src/QMS.Core/wwwroot/editor_fixes.js
+++ src/QMS.Core/wwwroot/editor_fixes.js
@@ -16,15 +16,11 @@
     }
 };
 
-const markdownEditors = [];
 function editorChange() {
     for (let x in editor.editors) {
         const e = editor.editors[x];
         if (e._fixed) continue;
         e._fixed = true;
-
-        if (e.schema.format == 'url' && e.schema && e.schema.links)
-            fixUploader(e);
 
         if (e.simplemde_instance)
             markdownEditors.push(e.simplemde_instance);
@@ -34,11 +30,4 @@ function editorChange() {
 addEventListener('DOMContentLoaded', () => {
     if (!window.editor) return;
     window.editor.on('change', editorChange);
-
-    const tabs = document.querySelectorAll('[data-toggle="tab"]');
-    for (let i = 0; i < tabs.length; i++) {
-        tabs[i].addEventListener('click', () => {
-            setTimeout(() => markdownEditors.forEach(e => e.cleanBlock()), 100)
-        })
-    }
 });
